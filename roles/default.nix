8c36133523f103bbcd6e6d62179ceb25395c243d
Kreativmonkey <kreativmonkey@calyrium.org>
2021-09-15 09:11:12 +0200

    add mosh

--- roles/default.nix
+++ roles/default.nix
@@ -25,27 +25,28 @@
         hunspell
         hunspellDicts.de_DE
         hunspellDicts.en_US
+        mosh
     ];
 
     programs.neovim = {
-	enable = true;
-	defaultEditor = true;
-	viAlias = true;
-	vimAlias = true;
-	configure = {
-        packages.myPlugins = with pkgs.vimPlugins; {
-        start = [
-          vim-surround # Shortcuts for setting () {} etc.
-          coc-git coc-highlight coc-python coc-rls coc-vetur coc-vimtex coc-yaml coc-html coc-json # auto completion
-          vim-nix # nix highlight
-          vimtex # latex stuff
-          fzf-vim # fuzzy finder through vim
-          nerdtree # file structure inside nvim
-          rainbow # Color parenthesis
-        ];
-	opt = [];
-	};	
-	};
+        enable = true;
+        defaultEditor = true;
+        viAlias = true;
+        vimAlias = true;
+        configure = {
+            packages.myPlugins = with pkgs.vimPlugins; {
+            start = [
+            vim-surround # Shortcuts for setting () {} etc.
+            coc-git coc-highlight coc-python coc-rls coc-vetur coc-vimtex coc-yaml coc-html coc-json # auto completion
+            vim-nix # nix highlight
+            vimtex # latex stuff
+            fzf-vim # fuzzy finder through vim
+            nerdtree # file structure inside nvim
+            rainbow # Color parenthesis
+            ];
+            opt = [];
+            };	
+        };
     };
 
 }
