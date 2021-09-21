e83037e21c4879b5d7261af3a5827f1504a8e4c3
Sebastian Preisner <kreativmonkey@calyrium.org>
2021-09-21 15:16:36 +0200

    start with grafana

---
+++ modules/prometheus.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, ... }: 
+{
+  # grafana configuration
+  services.grafana = {
+    enable = true;
+    domain = "grafana.oc4.de";
+    port = 2342;
+    addr = "127.0.0.1";
+  };
+  
+  # nginx reverse proxy
+  services.nginx.virtualHosts.${config.services.grafana.domain} = {
+    locations."/" = {
+        proxyPass = "http://127.0.0.1:${toString config.services.grafana.port}";
+        proxyWebsockets = true;
+    };
+  };
+}
