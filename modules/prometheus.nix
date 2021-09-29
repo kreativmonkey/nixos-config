22b12030a8b6bf04bfc9b9e9767a1c9b30b14667
Pegasus <kreativmonkey@caylrium.org>
2021-09-29 07:01:11 +0000

    working version of prometheus

--- modules/prometheus.nix
+++ modules/prometheus.nix
@@ -10,9 +10,52 @@
   
   # nginx reverse proxy
   services.nginx.virtualHosts.${config.services.grafana.domain} = {
+    ## Force HTTP redirect to HTTPS
+    forceSSL = true;
+    ## LetsEncrypt
+    enableACME = true;
+
     locations."/" = {
         proxyPass = "http://127.0.0.1:${toString config.services.grafana.port}";
         proxyWebsockets = true;
+    };
+  };
+
+  services.prometheus = {
+    enable = true;
+    port = 9001;
+
+    exporters = {
+      node = {
+        enable = true;
+        enabledCollectors = ["systemd"];
+        port = 9002;
+      };
+    };
+
+    scrapeConfigs = [
+      {
+        job_name = "pegasus";
+        static_configs = [{
+          targets = [ "127.0.0.1:${toString config.services.prometheus.exporters.node.port}" ];
+        }];
+      }
+    ];
+  };
+
+  services.loki = {
+    enable = true;
+    configFile = ./loki-local-config.yaml;
+  };
+
+  systemd.services.promtail = {
+    description = "Promtail service for Loki";
+    wantedBy = ["multi-user.target"];
+
+    serviceConfig = {
+      ExecStart = ''
+        ${pkgs.grafana-loki}/bin/promtail --config.file ${./promtail.yaml}
+        '';
     };
   };
 }
