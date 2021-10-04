ab210990e07f09b58bf86c96189730c6d294d9b2
Pegasus <kreativmonkey@caylrium.org>
2021-10-04 13:01:05 +0000

    postgresql vorbereitung

--- modules/vikunja.nix
+++ modules/vikunja.nix
@@ -1,4 +1,4 @@
-{config, pkgs, lib,...}:
+{config, pkgs, lib, ...}:
 let
   unstable = import <nixos-unstable> {};
 in
@@ -12,11 +12,60 @@ in
       frontendHostname = "todo.oc4.de";
       package-frontend = unstable.vikunja-frontend;
       package-api = unstable.vikunja-api;
-
+      #database = {
+      #  type = "postgres";
+      #  host = "localhost";
+      #  database = "vikunja";
+      #  user = "vikunja";
+      #};
+      #environmentFiles = [
+      #  "/var/src/secrets/vikunja_db_pass"
+      #];
+      settings = {
+        auth = {
+          local = {
+            enable = false;
+          };
+          openid = {
+            enable = true;
+            providers = [ {
+              name = "vikunja";
+              authurl = "https://login.oc4.de/application/o/vikunja";
+              clientid = lib.removeSuffix "\n" (builtins.readFile /var/src/secrets/vikunja_clientid);
+              clientsecret = lib.removeSuffix "\n" (builtins.readFile /var/src/secrets/vikunja_clientsecret);
+            } ];
+          };
+        };
+      };
     };
 
-    services.nginx.virtualHosts."todo.oc4.de" = {
+    services.nginx.virtualHosts."${config.services.vikunja.frontendHostname}" = {
       enableACME = true;
       forceSSL = true;
     };
+
+    services.postgresql = {
+      ensureDatabases = [ "vikunja" ];
+      ensureUsers = [ { 
+        name = "vikunja";
+        ensurePermissions = { "DATABASE vikunja" = "ALL PRIVILEGES"; };
+      } ];
+    };
+
+    systemd.services.vikunja-api = {
+      serviceConfig = {
+        DynamicUser = lib.mkForce false;
+        User = "vikunja";
+        Group = "vikunja";
+      };
+    };
+
+    users.users.vikunja = {
+      description = "Vikunja Service";
+      createHome = false;
+      group = "vikunja";
+      isSystemUser = true;
+    };
+
+    users.groups.vikunja = {};
   }
