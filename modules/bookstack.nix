0f7a61760682c3dc2d38b8e577b65f14b682ad81
Pegasus <kreativmonkey@caylrium.org>
2021-10-05 05:39:32 +0000

    first only proxyPass

--- modules/bookstack.nix
+++ modules/bookstack.nix
@@ -1,36 +1,11 @@
-{pkgs, lib, ...}:
+{pkgs, ...}:
 {
-  services.httpd =
-  {
-    enable = true;
-    enablePHP = true;
-    adminAddr = "admin@email.tld";
 
-    extraConfig = ''
-      DirectoryIndex index.html index.php
-    '';
-
-    virtualHosts = 
-    let
-    configFile = pkgs.writeText "config.inc.php" ''
-      <?php
-      /* Authentication type */
-      $cfg['Servers'][1]['auth_type'] = 'http';
-      /* Server parameters */
-      $cfg['Servers'][1]['host'] = 'localhost';
-      $cfg['Servers'][1]['compress'] = false;
-      $cfg['Servers'][1]['AllowNoPassword'] = false;
-    '';
-    phpmyadminPkg = pkgs.callPackage ./pkg.nix { inherit configFile; };
-    phpmyadmin = {
-      hostName = "phpmyadmin.tld";
-      documentRoot = phpmyadminPkg;
-      extraConfig = ''
-        <Directory "${phpmyadminPkg}">
-          Options -Indexes
-        </Directory>
-      '';
-    };   
-   in [phpmyadmin];
+  services.nginx.virtualHosts."wiki.oc4.de" = {
+    enableACME = true;
+    forceSSL = true;
+    locations."/" = {
+      proxyPass = "http://localhost:6875";
+    };
   };
 }
