8b61a9a779780c3c2be990933a993eed96ef4ff5
Sebastian Preisner <kreativmonkey@calyrium.org>
2021-10-05 13:53:36 +0200

    remove curly breackets

--- modules/bookstack.nix
+++ modules/bookstack.nix
@@ -7,16 +7,16 @@
     appURL = "https://wiki.oc4.de";
     appKeyFile = /var/src/secrets/bookstack_appkey;
     extraConfig = ''
-      AUTH_METHOD=saml2;
-      SAML2_NAME=SSO;
-      SAML2_EMAIL_ATTRIBUTE=email;
-      SAML2_EXTERNAL_ID_ATTRIBUTE=uid;
-      SAML2_DISPLAY_NAME_ATTRIBUTES=Name;
-      SAML2_IDP_ENTITYID=https://login.oc4.de/api/v3/providers/saml/8/metadata/?download;
-      SAML2_AUTOLOAD_METADATA=true;
-      SAML2_USER_TO_GROUPS=true;
-      SAML2_GROUP_ATTRIBUTE=groups;
-      SAML2_REMOVE_FROM_GROUPS=true;
+      AUTH_METHOD=saml2
+      SAML2_NAME=SSO
+      SAML2_EMAIL_ATTRIBUTE=email
+      SAML2_EXTERNAL_ID_ATTRIBUTE=uid
+      SAML2_DISPLAY_NAME_ATTRIBUTES=Name
+      SAML2_IDP_ENTITYID=https://login.oc4.de/api/v3/providers/saml/8/metadata/?download
+      SAML2_AUTOLOAD_METADATA=true
+      SAML2_USER_TO_GROUPS=true
+      SAML2_GROUP_ATTRIBUTE=groups
+      SAML2_REMOVE_FROM_GROUPS=true
     '';
 
     nginx = {
