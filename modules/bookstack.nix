ffd087179114af70756349ad0c5df31646aa9ee1
Pegasus <kreativmonkey@caylrium.org>
2021-10-05 06:44:20 +0000

    working version

--- modules/bookstack.nix
+++ modules/bookstack.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}:
+{pkgs, config, ...}:
 {
   services.bookstack = {
     enable = true;
@@ -6,18 +6,18 @@
     group = "bookstack";
     appURL = "https://wiki.oc4.de";
     appKeyFile = /var/src/secrets/bookstack_appkey;
-    extraConfig = {
-      AUTH_METHOD="saml2";
-      SAML2_NAME="SSO";
-      SAML2_EMAIL_ATTRIBUTE="email";
-      SAML2_EXTERNAL_ID_ATTRIBUTE="uid";
-      SAML2_DISPLAY_NAME_ATTRIBUTES="Name";
-      SAML2_IDP_ENTITYID="https://login.oc4.de/api/v3/providers/saml/8/metadata/?download";
+    extraConfig = ''
+      AUTH_METHOD=saml2;
+      SAML2_NAME=SSO;
+      SAML2_EMAIL_ATTRIBUTE=email;
+      SAML2_EXTERNAL_ID_ATTRIBUTE=uid;
+      SAML2_DISPLAY_NAME_ATTRIBUTES=Name;
+      SAML2_IDP_ENTITYID=https://login.oc4.de/api/v3/providers/saml/8/metadata/?download;
       SAML2_AUTOLOAD_METADATA=true;
       SAML2_USER_TO_GROUPS=true;
-      SAML2_GROUP_ATTRIBUTE="groups";
+      SAML2_GROUP_ATTRIBUTE=groups;
       SAML2_REMOVE_FROM_GROUPS=true;
-    };
+    '';
 
     nginx = {
       serverName = "wiki.oc4.de";
@@ -29,7 +29,7 @@
       user = config.services.bookstack.user;
       name = "bookstack";
       host = "localhost";
-      passwordFile = /var/src/secrets/bookstack_db_pass;
+      #passwordFile = /var/src/secrets/bookstack_db_pass;
       createLocally = true;
     };
   };
